bf59b7513663da7d142bee41eb42db1639a06bfe
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-07-27 02:14:44 +0200

    outcomment + pca

--- session_4/section_2/script.py
+++ session_4/section_2/script.py
@@ -27,30 +27,30 @@ plt.show()
 
 import umap
 
-# reducer = umap.UMAP()
-# 
-# penguin_data = penguins[
-#     [
-#         "culmen_length_mm",
-#         "culmen_depth_mm",
-#         "flipper_length_mm",
-#         "body_mass_g",
-#     ]
-# ].values
-# scaled_penguin_data = StandardScaler().fit_transform(penguin_data)
-# 
-# 
-# embedding = reducer.fit_transform(scaled_penguin_data)
-# embedding.shape
-# 
-# 
-# plt.scatter(
-#     embedding[:, 0],
-#     embedding[:, 1],
-#     c=[sns.color_palette()[x] for x in penguins.species_short.map({"Adelie":0, "Chinstrap":1, "Gentoo":2})])
-# plt.gca().set_aspect('equal', 'datalim')
-# plt.title('UMAP projection of the Penguin dataset', fontsize=24)
-# plt.show()
+reducer = umap.UMAP()
+
+penguin_data = penguins[
+    [
+        "culmen_length_mm",
+        "culmen_depth_mm",
+        "flipper_length_mm",
+        "body_mass_g",
+    ]
+].values
+scaled_penguin_data = StandardScaler().fit_transform(penguin_data)
+
+
+embedding = reducer.fit_transform(scaled_penguin_data)
+embedding.shape
+
+
+plt.scatter(
+    embedding[:, 0],
+    embedding[:, 1],
+    c=[sns.color_palette()[x] for x in penguins.species_short.map({"Adelie":0, "Chinstrap":1, "Gentoo":2})])
+plt.gca().set_aspect('equal', 'datalim')
+plt.title('UMAP projection of the Penguin dataset', fontsize=24)
+plt.show()
 
 # in comparison to PCA:
 from sklearn.decomposition import PCA
