c447d0b6655e274950c3596b893cffd7a90fb07b
Tilman Kerl <tilman.kerl@uni-konstanz.de>
2021-08-01 01:59:22 +0200

    update

--- session_4/section_2/tasks.py
+++ session_4/section_2/tasks.py
@@ -12,3 +12,37 @@
 # df[['hour', "minute"]] = df.time.str.split(":", expand=True)
 # Include location, price, last4ccnum and maybe hour
 
+import numpy as np
+from sklearn.datasets import load_digits
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import umap
+
+reducer = umap.UMAP()
+
+df = pd.read_csv("data/cc_data_2.csv")
+print(df)
+
+data = df[
+    [
+        "hour",
+        "price",
+        "last4ccnum",
+    ]
+].values
+scaled_data = StandardScaler().fit_transform(data)
+
+
+embedding = reducer.fit_transform(scaled_data)
+embedding.shape
+
+
+plt.scatter(
+    embedding[:, 0],
+    embedding[:, 1])
+plt.gca().set_aspect('equal', 'datalim')
+plt.title('UMAP projection of the CC dataset', fontsize=24)
+plt.show()
